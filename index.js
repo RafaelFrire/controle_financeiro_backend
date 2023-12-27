921f4548539a7e1cc0b0ac1b1d9b37fffb3123d6
RafaelFrire <rafaelfesousa@hotmail.com>
2023-12-27 16:22:50 -0300

    cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const db = require('./data')
 const port = 3000;
+const cors = require("cors");
 const app = express();
 
-app.use(express.json())
 
+app.use(express.json());
+app.use(cors());
 
 app.get('/transactions', (req, res) =>{
     try{
