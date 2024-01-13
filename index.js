878bf821b48148e6a8e1164cb08b11aa7d252e0a
RafaelFrire <rafaelfesousa@hotmail.com>
2024-01-13 19:14:58 -0300

    deploy vercel

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./data')
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 const app = express();
 
