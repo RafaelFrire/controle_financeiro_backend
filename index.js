0d83ee08600ad893dbe1557173394186216dd645
RafaelFrire <rafaelfesousa@hotmail.com>
2024-01-13 19:32:12 -0300

    headers cors

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
+
 app.get('/transactions', (req, res) =>{
     try{
         const data = db.db
