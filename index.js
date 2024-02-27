d0b755389944bb12fe3a17a9939746483176faa9
RafaelFrire <rafaelfesousa@hotmail.com>
2024-02-27 20:32:44 -0300

    update transictions routes

--- index.js
+++ index.js
@@ -16,52 +16,7 @@ app.use((req, res, next) => {
     next();
   });
 
-app.get('/transactions', (req, res) =>{
-    try{
-        const data = db.db
-        res.status(200).json(data)
-    }
-    catch (err){
-        console.error("erro:", err.message);
-        res.status(500).send("erro interno do servidor")
-    }
-   
-})
-
-
-app.post('/transactions', (req, res) =>{
-    try{
-        db.insertAtDB(req.body)
-        res.status(201)
-        res.end();
-    }
-    catch (err){
-        console.error("error", err.message)
-        res.sendStatus(500);
-    }
-   
-})
-
-app.delete('/transactions/:id', (req, res) =>{
-    const id = req.params.id
-    try{
-       const removed = db.removeAtDB(id)
- 
-       if(removed){
-            res.status(200).send("removido com sucesso!")
-        }
-        else{
-            res.status(204).send(`O ID:${id} não encontrado`)
-        }
-     
-    }
-    catch (err){
-        console.error(err)
-    }
-    finally{
-        res.end()
-    }
-})
+app.use('/', transictionsRoutes)
 
 
 
